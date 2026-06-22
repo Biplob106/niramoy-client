523914c4eae809542dd674314b08e7ff90979366
Biplob106 <biplobsd106@gmail.com>
2026-06-22 21:43:58 +0600

    feat: add responsive navbar with auth state and logout

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import AuthProvider from "@/providers/AuthProvider";
 
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
 import { Toaster } from "react-hot-toast";
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,11 +26,13 @@ export default function RootLayout({ children }) {
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body suppressHydrationWarning>
-  <AuthProvider>
-    {children}
-      <Toaster position="top-center" />
-  </AuthProvider>
-</body>
+       
+        <AuthProvider>
+         <Navbar />
+          {children}
+          <Toaster position="top-center" />
+        </AuthProvider>
+      </body>
     </html>
   );
 }
